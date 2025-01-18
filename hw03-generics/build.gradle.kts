865b18d673779c03434bb7dae86d2d5b4384b0b4
Andrei Gelei <a.geley@gmail.com>
2025-01-18 21:01:50 +0300

    feat(hw03-generics): id only in EqualsAndHashCode

--- hw03-generics/build.gradle.kts
+++ hw03-generics/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     implementation ("ch.qos.logback:logback-classic")
+    compileOnly ("org.projectlombok:lombok")
+    annotationProcessor ("org.projectlombok:lombok")
 
     testImplementation ("org.junit.jupiter:junit-jupiter-api")
     testImplementation ("org.junit.jupiter:junit-jupiter-engine")
--- hw03-generics/src/main/java/homework/Customer.java
+++ hw03-generics/src/main/java/homework/Customer.java
@@ -1,62 +1,25 @@
 package homework;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+@AllArgsConstructor
 public class Customer {
+    @EqualsAndHashCode.Include
     private final long id;
+
+    @Setter
     private String name;
+
+    @Setter
     private long scores;
-
-    // todo: 1. в этом классе надо исправить ошибки
-
-    public Customer(long id, String name, long scores) {
-        this.id = id;
-        this.name = name;
-        this.scores = scores;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getScores() {
-        return scores;
-    }
-
-    public void setScores(long scores) {
-        this.scores = scores;
-    }
 
     @Override
     public String toString() {
         return "Customer{" + "id=" + id + ", name='" + name + '\'' + ", scores=" + scores + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Customer customer = (Customer) o;
-
-        if (id != customer.id) return false;
-        if (scores != customer.scores) return false;
-        return Objects.equals(name, customer.name);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Long.hashCode(id);
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + Long.hashCode(scores);
-        return result;
     }
 }
